d92903686863b6d11d789a01bff7f0bf5c24c556
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-05-29 23:28:39 -0300

    ci: providing service and use case on user module

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -2,11 +2,17 @@ import { Module } from '@nestjs/common'
 import { RegisterController } from './controllers/register.controller'
 import { MongooseModule } from '@nestjs/mongoose'
 import { User, UserSchema } from './schemas/user.schema'
+import { UserService } from './services/user.service'
+import { RegisterUseCase } from './usecases/register.usecase'
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
   controllers: [RegisterController],
+  providers: [
+    { provide: 'S_USER_SERVICE', useClass: UserService },
+    { provide: 'U_REGISTER_USECASE', useClass: RegisterUseCase },
+  ],
 })
 export class UserModule {}
