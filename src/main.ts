dbfc6a88cd79eb9a2c1fd4ba290f6dbc21d8fddb
Gabriel Araujo Lima <gabriel.araujo2902@outlook.com>
2024-05-30 15:40:17 -0300

    ci: cors usage

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module'
 import { ENVIROMENT } from './config/enviroments'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule, { cors: true })
   await app.listen(ENVIROMENT.PORT)
 }
 bootstrap()
