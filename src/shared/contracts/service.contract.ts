0432030fcca1fd99da1d1886d0e51509897d16b2
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-05-29 22:37:19 -0300

    chore: anything can be in return

--- src/shared/contracts/service.contract.ts
+++ src/shared/contracts/service.contract.ts
@@ -1,6 +1,6 @@
-export interface IServiceContract<Input, Output> {
-  find: (dto: Input) => Promise<Output>
-  findAll?: () => Promise<Output>
+export interface IServiceContract<Input> {
+  find: (dto: Input) => Promise<any>
+  findAll?: () => Promise<any>
   create: (dto: Input) => Promise<boolean>
   remove: (dto: Input) => Promise<boolean>
 }
