1ba40bb323f1018c5246ecfa8388aee414dcba98
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-05-29 23:57:09 -0300

    ci: implementation of use case at user module

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -4,15 +4,18 @@ import { MongooseModule } from '@nestjs/mongoose'
 import { User, UserSchema } from './schemas/user.schema'
 import { UserService } from './services/user.service'
 import { RegisterUseCase } from './usecases/register.usecase'
+import { LoginController } from './controllers/login.controller'
+import { LoginUseCase } from './usecases/login.usecase'
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
-  controllers: [RegisterController],
+  controllers: [RegisterController, LoginController],
   providers: [
     { provide: 'S_USER_SERVICE', useClass: UserService },
     { provide: 'U_REGISTER_USECASE', useClass: RegisterUseCase },
+    { provide: 'U_LOGIN_USECASE', useClass: LoginUseCase },
   ],
 })
 export class UserModule {}
