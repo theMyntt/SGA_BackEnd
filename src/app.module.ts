33fc1d012285a682f33396d3217aeec2f47eeaac
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-05-29 22:41:28 -0300

    feat: connecting to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserModule } from './modules/user/user.module';
+import { Module } from '@nestjs/common'
+import { UserModule } from './modules/user/user.module'
+import { MongooseModule } from '@nestjs/mongoose'
+import { ENVIROMENT } from './config/enviroments'
 
 @Module({
-  imports: [UserModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [MongooseModule.forRoot(ENVIROMENT.MONGODB_URI), UserModule],
 })
 export class AppModule {}
