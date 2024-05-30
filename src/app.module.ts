29bda8ff20fef65101737b8dd9c39cc9bbbadf02
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-05-30 13:28:37 -0300

    ci: a module for modules folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common'
 import { UserModule } from './modules/user/user.module'
 import { MongooseModule } from '@nestjs/mongoose'
 import { ENVIROMENT } from './config/enviroments'
+import { BaseModule } from './modules/base.module'
 
 @Module({
-  imports: [MongooseModule.forRoot(ENVIROMENT.MONGODB_URI), UserModule],
+  imports: [MongooseModule.forRoot(ENVIROMENT.MONGODB_URI), BaseModule],
 })
 export class AppModule {}
