87c3f171b29e74cc5aeaf1c20575d7c414f5fab5
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-05-30 00:02:42 -0300

    test: cpfValidator unit test

---
+++ test/utils/cpfValidator.util.spec.ts
@@ -0,0 +1,23 @@
+import { cpfValidator } from '../../src/shared/utils/cpfValidator.util'
+
+describe('cpfValidator', () => {
+  test('Should return true for a valid CPF', () => {
+    const cpf = '529.982.247-25'
+    expect(cpfValidator(cpf)).toBe(true)
+  })
+
+  test('Should return false for an invalid CPF', () => {
+    const cpf = '111.111.111-11'
+    expect(cpfValidator(cpf)).toBe(false)
+  })
+
+  test('Should return false for a CPF with less than 11 digits', () => {
+    const cpf = '123456789'
+    expect(cpfValidator(cpf)).toBe(false)
+  })
+
+  test('Should return false for a CPF with all digits equal', () => {
+    const cpf = '000.000.000-00'
+    expect(cpfValidator(cpf)).toBe(false)
+  })
+})
