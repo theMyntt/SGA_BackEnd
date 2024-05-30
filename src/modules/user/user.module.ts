7baae2890345267465088d920e379be60f66e8c3
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-05-29 22:37:50 -0300

    feat: implementation of schema

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,7 +1,12 @@
-import { Module } from '@nestjs/common';
-import { RegisterController } from './controllers/register.controller';
+import { Module } from '@nestjs/common'
+import { RegisterController } from './controllers/register.controller'
+import { MongooseModule } from '@nestjs/mongoose'
+import { User, UserSchema } from './schemas/user.schema'
 
 @Module({
-  controllers: [RegisterController]
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
+  controllers: [RegisterController],
 })
 export class UserModule {}
