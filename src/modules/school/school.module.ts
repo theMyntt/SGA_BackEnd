35494ad589b13fe93cf5339e6c7a341c1aa28ca7
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-05-31 20:58:18 -0300

    chore: school module

--- src/modules/school/school.module.ts
+++ src/modules/school/school.module.ts
@@ -1,4 +1,18 @@
-import { Module } from '@nestjs/common';
+import { Module } from '@nestjs/common'
+import { MongooseModule } from '@nestjs/mongoose'
+import { School, SchoolSchema } from './schemas/school.schema'
+import { SchoolService } from './services/school.service'
+import { RegisterController } from './controllers/register.controller'
+import { RegisterUseCase } from './usecases/register.usecase'
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: School.name, schema: SchoolSchema }]),
+  ],
+  controllers: [RegisterController],
+  providers: [
+    { provide: 'S_SCHOOL_SERVICE', useClass: SchoolService },
+    { provide: 'U_REGISTER_USECASE', useClass: RegisterUseCase },
+  ],
+})
 export class SchoolModule {}
