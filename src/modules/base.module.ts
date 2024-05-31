7a2593c42f708df89c49adfb871efeda55723364
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-05-31 20:34:17 -0300

    feat: basic school module

--- src/modules/base.module.ts
+++ src/modules/base.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common'
 import { UserModule } from './user/user.module'
+import { SchoolModule } from './school/school.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, SchoolModule],
 })
 export class BaseModule {}
