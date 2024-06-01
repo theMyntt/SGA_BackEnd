6d90e03d68945cbe841d6880661e68d6ad6e469c
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-06-01 17:45:07 -0300

    chore: implementation of controller and use case at school module

--- src/modules/school/school.module.ts
+++ src/modules/school/school.module.ts
@@ -4,15 +4,18 @@ import { School, SchoolSchema } from './schemas/school.schema'
 import { SchoolService } from './services/school.service'
 import { RegisterController } from './controllers/register.controller'
 import { RegisterUseCase } from './usecases/register.usecase'
+import { PickUseCase } from './usecases/pick.usecase'
+import { PickController } from './controllers/pick.controller'
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: School.name, schema: SchoolSchema }]),
   ],
-  controllers: [RegisterController],
+  controllers: [RegisterController, PickController],
   providers: [
     { provide: 'S_SCHOOL_SERVICE', useClass: SchoolService },
     { provide: 'U_REGISTER_USECASE', useClass: RegisterUseCase },
+    { provide: 'U_PICK_USECASE', useClass: PickUseCase },
   ],
 })
 export class SchoolModule {}
