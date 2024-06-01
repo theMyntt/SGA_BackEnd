405b74112f7a50a9cb57b78bf6eabc48cbb6f12e
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-06-01 17:48:37 -0300

    refactor: remove semicolon

--- src/modules/base.module.ts
+++ src/modules/base.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common'
 import { UserModule } from './user/user.module'
-import { SchoolModule } from './school/school.module';
+import { SchoolModule } from './school/school.module'
 
 @Module({
   imports: [UserModule, SchoolModule],
