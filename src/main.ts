b5078338d7c6f79ad11b66e6f2252cd492f3bb19
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-06-12 20:55:03 -0300

    ci: swagger basic configuration

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ENVIROMENT } from './config/enviroments'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true })
+
+  const config = new DocumentBuilder()
+    .setTitle('SGA API')
+    .setDescription('SGA API Swagger Documentation')
+    .setVersion('0.0.1')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+
+  SwaggerModule.setup('api/docs', app, document)
+
   await app.listen(ENVIROMENT.PORT)
 }
 bootstrap()
